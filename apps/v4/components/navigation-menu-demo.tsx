3d74fdfd6d611eef2190bd52a8c4686e599813a0
Ahsan Najam <ahsannajam@hotmail.com>
2025-05-11 23:53:29 +0500

    changes

--- apps/v4/components/navigation-menu-demo.tsx
+++ apps/v4/components/navigation-menu-demo.tsx
@@ -61,9 +61,9 @@ export function NavigationMenuDemo() {
               <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                 <li className="row-span-3">
                   <NavigationMenuLink asChild>
-                    <a
+                    <Link href="/"
                       className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-6 no-underline outline-hidden select-none focus:shadow-md"
-                      href="/"
+
                     >
                       <div className="mt-4 mb-2 text-lg font-medium">
                         shadcn/ui
@@ -71,7 +71,7 @@ export function NavigationMenuDemo() {
                       <p className="text-muted-foreground text-sm leading-tight">
                         Beautifully designed components built with Tailwind CSS.
                       </p>
-                    </a>
+                    </Link>
                   </NavigationMenuLink>
                 </li>
                 <ListItem href="/docs" title="Introduction">
